2153b6117c030518ac0b67ff6f54c7d5fd5ac20c
Chris Sauter <sauterdev@gmail.com>
2023-01-20 21:44:40 -0500

    rework to eliminate array usage in addBook method. try and create table row with new method

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 // to do list
-// is addbook method too long?
 // mark read method on library class
-// remove book method on library class with button
+// remove book method on library class
 // reindex when book is removed
 // check box for read/not read
-
 
 class Book {
   constructor(id, title, author, read) {
@@ -18,34 +16,36 @@ class Book {
 class Library {
   bookList = [];
   bookCount = 0;
+  bookPosition = 0; //identifies position when modals are created
 
   markRead(param) {}
 
   addBook = () => {
     this.bookCount++;
     //build array to easier create table elements with loop
-    let book = [ 
+    let book = [
       this.bookCount,
       titleInput.value,
       authorInput.value,
       readYesNo.checked,
-    ]; 
+    ];
     let newBook = new Book(book[0], book[1], book[2], book[3]);
     this.bookList.push(newBook);
 
     let newTr = document.createElement(`tr`);
     newTr.className = `bookLineItem`;
-    newTr.setAttribute(`whichBook`, `${book[0]}`) //add attribute to pull ID number off html TR event listener
+    newTr.setAttribute(`whichBook`, `${book[0]}`); //add attribute to pull ID number off html TR event listener
     for (let i = 0; i < book.length; i++) {
-      //creates 3 tables elements to add to newTr 
+      //creates 3 tables elements to add to newTr
       let newTd = document.createElement(`td`);
       newTd.textContent = book[i];
       newTr.appendChild(newTd);
-      if(i == book.length - 1) { // add check box to last table row
+      if (i == book.length - 1) {
+        // add check box to last table row
         let checkButton = document.createElement(`input`);
         checkButton.type = `checkbox`;
         checkButton.name = `read`;
-        checkButton.id = `readLibraryCheckbox`
+        checkButton.id = `readLibraryCheckbox`;
         checkButton.checked = readYesNo.checked;
         checkButton.disabled = true;
         newTd.appendChild(checkButton);
@@ -53,49 +53,66 @@ class Library {
       }
     }
     tableBody.appendChild(newTr);
-    
-     newTr.addEventListener(`click`, chrisLibrary.buildModal);//creates a listener on the book list table row
+
+    newTr.addEventListener(`click`, chrisLibrary.buildModal); //creates a listener on the book list table row
 
     titleInput.value = ""; //resets input fields
     authorInput.value = "";
-    readYesNo.checked = false; 
+    readYesNo.checked = false;
     submitButton.disabled = true;
   };
 
-   buildModal = (index, title, author, read) => {
-   let bookPosition = event.target.parentNode.getAttribute(`whichBook`);
-   console.log(chrisLibrary);
-  const  modalDiv = document.createElement(`div`);
-  modalDiv.id = `modal`;
-  const  modalCard = document.createElement(`div`);
-  modalCard.className = `modal-card`;
-  const removeModal = document.createElement(`button`);
-  removeModal.id = `removeModal`;
-  removeModal.textContent = "Put Back";
-  const discardBook = document.createElement(`button`);
-  discardBook.id = `discardBook`;
-  discardBook.textContent = "Discard Book";
-  const markRead = document.createElement(`button`);
-  markRead.id = `markRead`;
-  markRead.textContent = "Mark Read";
-  const modalPara = document.createElement(`p`);
-  modalPara.textContent = `${this.bookList[bookPosition - 1].title}, by ${this.bookList[bookPosition - 1].author}`; //displays the name of the book and the author in the paragraph tab
-  
-  contentDiv.appendChild(modalDiv);
-  modalDiv.appendChild(modalCard);
-  modalCard.appendChild(removeModal);
-  modalCard.appendChild(discardBook);
-  modalCard.appendChild(markRead);
-  modalCard.appendChild(modalPara);
+  buildModal = () => {
+    this.bookPosition = event.target.parentNode.getAttribute(`whichBook`);
+    const modalDiv = document.createElement(`div`);
+    modalDiv.id = `modal`;
+    const modalCard = document.createElement(`div`);
+    modalCard.className = `modal-card`;
+    const removeModal = document.createElement(`button`);
+    removeModal.id = `removeModal`;
+    removeModal.textContent = "Put Back";
+    const removeBook = document.createElement(`button`);
+    removeBook.id = `removeBook`;
+    removeBook.textContent = "Remove Book";
+    const markRead = document.createElement(`button`);
+    markRead.id = `markRead`;
+    markRead.textContent = "Mark Read";
+    const modalPara = document.createElement(`p`);
+    modalPara.textContent = `${this.bookList[this.bookPosition - 1].title}, by ${
+      this.bookList[this.bookPosition - 1].author //displays the name of the book and the author in the paragraph tab
+    }`; 
 
-  
-  const removeModalAction = document.querySelector(`#removeModal`);
-  removeModalAction.addEventListener(`click`, hide);
-  
-  function hide(event) {
+    contentDiv.appendChild(modalDiv);
+    modalDiv.appendChild(modalCard);
+    modalCard.appendChild(removeModal);
+    modalCard.appendChild(removeBook);
+    modalCard.appendChild(markRead);
+    modalCard.appendChild(modalPara);
+
+    //should this be its own method? YES to remove when hitting other buttons
+    const removeModalAction = document.querySelector(`#removeModal`);
+    removeModalAction.addEventListener(`click`, hideModal);
+
+    function hideModal(event) {
       contentDiv.removeChild(modalDiv);
-  }
-  }
+    }
+
+    removeBook.addEventListener(`click`, chrisLibrary.discardBook)
+  };
+
+
+  discardBook = () => {
+    console.log(this.bookList)
+    console.log(this.bookCount)
+    console.log(this.bookPosition)
+    this.bookList.splice((this.bookPosition - 1), 1);
+    console.log(this.bookList)
+    console.log(this.bookCount)
+    console.log(this.bookPosition)
+
+
+  }    
+
 }
 
 let chrisLibrary = new Library();
@@ -109,6 +126,7 @@ const contentDiv = document.querySelector(`.content`);
 
 submitButton.addEventListener(`click`, chrisLibrary.addBook);
 
+
 //function runs on key up events for two input fields. If either is empty, the `add-book` button is disabled
 function enableSubmit() {
   let inputs = document.getElementsByClassName(`required-input`);
@@ -121,5 +139,3 @@ function enableSubmit() {
   }
   submitButton.disabled = !isValid;
 }
-
-
